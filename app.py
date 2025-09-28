2848a221903d94d9a81565db0fa684b10740852f
Pramod  Rathnayake <rnmpramod@gmail.com>
2025-09-29 00:53:35 +0530

    Updated

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
 import gradio as gr
 from retriever import get_retriver
 from generate import generate_response
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
 
 def chat(query):
-    if not query.strip():
-        return "Please ask a question!"
+    try:
+        if not query.strip():
+            return "Please ask a question!"
+        
+        chunks = get_retriver(query)
+        answer = generate_response(query, chunks)
+        
     
-    chunks = get_retriver(query)
-    answer = generate_response(query, chunks)
-    
-  
-    sources = "\n".join([f"- {c['source']} (score: {c['score']:.2f})" for c in chunks])
-    full_response = f"{answer}\n\n**Sources:**\n{sources}"
-    
-    return full_response
-
-
+        sources = "\n".join([f"- {c['source']} (score: {c['score']:.2f})" 
+                             if isinstance(c.get('score'), (int, float))
+                             else f"- {c['source']} (score: {c.get('score', 'N/A')})" for c in chunks])
+        full_response = f"{answer}\n\n**Sources:**\n{sources}"
+        
+        return full_response
+        
+    except Exception as e:
+        logger.error(f"Error setting up chat: {str(e)}")  
+        
+        
 iface = gr.Interface(
-    fn=chat,
-    inputs=gr.Textbox(label="Ask your FAQ", placeholder="e.g., What is the refund policy?"),
-    outputs=gr.Textbox(label="Answer"),
-    title="Multi-Source FAQ Bot",
-    description="Powered by Phi-2 LLM and RAG from your sources."
-)
-
+            fn=chat,
+            inputs=gr.Textbox(label="Ask your FAQ", placeholder="e.g., What do you want to know?"),
+            outputs=gr.Textbox(label="Answer"),
+            title="Multi-Source FAQ Bot",
+            description="Powered by Phi-2 LLM."
+        )
 if __name__ == "__main__":
     iface.launch(share=True) 
